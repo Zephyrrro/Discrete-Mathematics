b397e4eefcd3bf50ded0ff7a8459f9f9d4238c48
cucu6 <1103452257@qq.com>
2019-11-25 18:01:22 +0800

    feat: experiment3

---
+++ experiment3/src/Calculate.js
@@ -0,0 +1,44 @@
+const getFactors = number => {
+  const factors = [];
+  const k = Math.round(Math.sqrt(number));
+  for (let i = 1; i <= k; i++) {
+    if (number % i === 0) {
+      if (i !== number / i) {
+        factors.push(i);
+        factors.push(number / i);
+      } else {
+        factors.push(i);
+      }
+    }
+  }
+  return factors.sort((first, second) => first - second);
+};
+
+const getCovers = factors => {
+  const covers = [];
+  const { length } = factors;
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (factors[j] % factors[i] === 0 && i < j) {
+        let flag = true;
+        for (let k = i + 1; k < j; k++) {
+          if (
+            factors[j] % factors[k] === 0 &&
+            factors[k] % factors[i] === 0 &&
+            i < k &&
+            k < j
+          ) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          covers.push(`<${factors[i]}, ${factors[j]}>`);
+        }
+      }
+    }
+  }
+  return covers;
+};
+
+module.exports = { getFactors, getCovers };
---
+++ experiment3/src/index.js
@@ -0,0 +1,23 @@
+const rl = require("readline-sync");
+const { getFactors, getCovers } = require("./Calculate");
+const Judge = require("./Judge");
+
+
+const n = Number(rl.question("输入一个正整数："));
+
+const factors = getFactors(n);
+const { length } = factors;
+console.log(`${n}一共有${length}个因子: [${factors}]`);
+
+
+const cover = getCovers(factors);
+console.log(`偏序集上的盖住关系：[${cover}]`);
+
+
+
+if(Judge(factors, n)) {
+  console.log("不是有补格.");
+} else {
+  console.log("是有补格.");
+}
+
